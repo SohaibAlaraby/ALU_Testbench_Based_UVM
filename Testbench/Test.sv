b9defdc3b0b509cf0d6674480fa77712d853390a
SohaibAlaraby <110909784+SohaibAlaraby@users.noreply.github.com>
2024-07-17 17:56:53 +0300

    Update Test.sv

--- Testbench/Test.sv
+++ Testbench/Test.sv
@@ -32,31 +32,10 @@ task run_phase(uvm_phase phase);
     `uvm_info(get_type_name(),"in ALU_Test run phase",UVM_LOW)
 
     phase.raise_objection(this);
-    /*
-    virtual task start(
-    uvm_sequencer_base sequencer,
-    uvm_sequence_base parent_sequence = null,
-    int this_priority = -1,
-    bit call_pre_post = 1
-    );
-    1-sequencer: The sequencer on which the sequence will run.
-    parent_sequence: Optional argument that controls 
-    2-whether pre_do, mid_do, and post_do are called in the parent sequence.
-    3-this_priority: Priority for the sequence (usually inherited from the parent sequence).
-    call_pre_post: Determines whether to 
-    4-call pre_body() and post_body() of the current sequence.
-    */
+   
     rst_seq = rst_sequence::type_id::create("rst_seq");
     rst_seq.start(env.alu_agt.alu_sequencer);
-    /*
-    rst_seq = base_sequence::type_id::create("rst_seq");
-    rst_seq = base_sequence::type_id::create("rst_seq",this);
-    The first line creates an isolated instance of the sequence.
-    The second line associates the sequence with a specific context (parent component),
-    allowing it to inherit properties or settings. 
-    This context-awareness can be beneficial in complex testbenches.
-    
-    */
+   
     repeat(80000) begin
         #20
         tst_seq= test_sequence::type_id::create("tst_seq");
